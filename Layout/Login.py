eb0755cb526bdf4f062b0bbdc8bd5b45eef21813
q25979 <q25979>
2019-04-11 14:15:29 +0800

    1

--- Layout/Login.py
+++ Layout/Login.py
@@ -8,9 +8,22 @@ class Layout(wx.Frame):
 		wx.Frame.__init__(self, parent, -1, "登录系统", size = (300,200))
 		self.InitUI()		
 		self.Centre()
+		self.SetBackgroundColour(wx.Colour(235,237,239))
 		self.Show()
 
 
 	def InitUI(self):
 		panel = wx.Panel(self)
-		wx.StaticText(panel, -1, "账号", pos = (20, 25))
+		wx.StaticText(panel, -1, "小名", pos = (33, 25))
+		wx.StaticText(panel, -1, "代理线", pos = (20, 65))
+
+		name = wx.TextCtrl(panel, -1, pos = (60, 22), size = (200, 22))
+		agent = wx.TextCtrl(panel, -1, pos = (60, 62), size = (200, 22))
+
+		btn_login = wx.Button(panel, -1, "登录", pos = (59, 98), size = (202, 26))
+		panel.Bind(wx.EVT_BUTTON, self.OnLogin, btn_login)
+
+
+	# 登录
+	def OnLogin(self, event):
+		wx.MessageBox("Hello")
